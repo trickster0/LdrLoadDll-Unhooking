8d655a8259a2c3b4af10634ff1286226f94848a3
trickster0 <tserpthanasis@gmail.com>
2022-01-16 16:17:36 +0200

    upload

---
+++ unhookLdrLoadDll/unhookLdrLoadDll.cpp
@@ -0,0 +1,101 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#define OBJ_CASE_INSENSITIVE 0x00000040L
+
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG           Length;
+	HANDLE          RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG           Attributes;
+	PVOID           SecurityDescriptor;
+	PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+#define InitializeObjectAttributes( i, o, a, r, s ) {    \
+      (i)->Length = sizeof( OBJECT_ATTRIBUTES );         \
+      (i)->RootDirectory = r;                            \
+      (i)->Attributes = a;                               \
+      (i)->ObjectName = o;                               \
+      (i)->SecurityDescriptor = s;                       \
+      (i)->SecurityQualityOfService = NULL;              \
+   }
+
+using pNewLdrLoadDll = NTSTATUS(NTAPI*)(PWCHAR PathToFile, ULONG Flags, PUNICODE_STRING ModuleFileName, PHANDLE ModuleHandle);
+
+
+PVOID CCopyMemory(PVOID Destination, CONST PVOID Source, SIZE_T Length)
+{
+	PBYTE D = (PBYTE)Destination;
+	PBYTE S = (PBYTE)Source;
+
+	while (Length--)
+		*D++ = *S++;
+
+	return Destination;
+}
+
+SIZE_T StringLengthW(LPCWSTR String)
+{
+	LPCWSTR String2;
+
+	for (String2 = String; *String2; ++String2);
+
+	return (String2 - String);
+}
+
+VOID RtlInitUnicodeString(PUNICODE_STRING DestinationString, PCWSTR SourceString)
+{
+	SIZE_T DestSize;
+
+	if (SourceString)
+	{
+		DestSize = StringLengthW(SourceString) * sizeof(WCHAR);
+		DestinationString->Length = (USHORT)DestSize;
+		DestinationString->MaximumLength = (USHORT)DestSize + sizeof(WCHAR);
+	}
+	else
+	{
+		DestinationString->Length = 0;
+		DestinationString->MaximumLength = 0;
+	}
+
+	DestinationString->Buffer = (PWCHAR)SourceString;
+}
+
+int main()
+{
+	pNewLdrLoadDll LdrLoadrDll;
+	UNICODE_STRING ldrldll;
+	OBJECT_ATTRIBUTES objectAttributes = { 0 };
+	wchar_t ldrstring[] = L"Wininet.dll";
+	RtlInitUnicodeString(&ldrldll, ldrstring);
+	InitializeObjectAttributes(&objectAttributes, &ldrldll, OBJ_CASE_INSENSITIVE, NULL, NULL);
+	unsigned char jumpPrelude[] = { 0x49, 0xBB };
+	unsigned char jumpAddress[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF };
+	unsigned char jumpEpilogue[] = { 0x41, 0xFF, 0xE3, 0xC3 };
+	LPVOID trampoline = VirtualAlloc(NULL,19, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	printf("Address of trampoline at 0x%p\n", trampoline);
+	LPVOID origLdrLoadDll = GetProcAddress(GetModuleHandleA("ntdll.dll"),"LdrLoadDll");
+	printf("Original LdrLoadDll at 0x%p\n",origLdrLoadDll);
+	CCopyMemory(trampoline,(PVOID)"\x48\x89\x5c\x24\x10", 5);
+	LPVOID jmpAddr = (void*)((char*)origLdrLoadDll + 0x5);
+	*(void**)(jumpAddress) = jmpAddr;
+	printf("Original jmp Address at 0x%p\n", jmpAddr);
+	CCopyMemory((PBYTE)trampoline+5, jumpPrelude, 2);
+	CCopyMemory((PBYTE)trampoline + 5 + 2, jumpAddress, sizeof(jumpAddress));
+	CCopyMemory((PBYTE)trampoline + 5 + 2 + 8, jumpEpilogue, 4);
+	DWORD oldProtect = 0;
+	VirtualProtect(trampoline,30,PAGE_EXECUTE_READ,&oldProtect);
+	LdrLoadrDll = (pNewLdrLoadDll)trampoline;
+	HANDLE wininetmodule = NULL;
+	LdrLoadrDll(NULL, 0 , &ldrldll, &wininetmodule);
+}
